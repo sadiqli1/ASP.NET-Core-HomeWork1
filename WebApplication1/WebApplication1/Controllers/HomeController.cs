fae93e3cbd2f8567c04698517f5c20908c0a6efe
sadiqli1 <isaas@code.edu.az>
2022-06-01 00:31:35 +0400

    created HomeWork

---
+++ WebApplication1/WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController:Controller
+    {
+        public ViewResult Index()
+        {
+            ViewResult view = new ViewResult();
+            view.ViewName = "HomePage";
+            return view;
+        }
+
+        public ViewResult About()
+        {
+            return new ViewResult{ViewName ="About"};
+        }
+
+        public ActionResult FAQ()
+        {
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            return View();
+        }
+    }
+}
---
+++ WebApplication1/WebApplication1/Controllers/ProdactController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class ProdactController:Controller
+    {
+        public ActionResult Details(int? id)
+        {
+            if(id == null)
+            {
+                return NotFound();
+            }
+            return Content(id.ToString());
+            //return View();
+        }
+    }
+}
